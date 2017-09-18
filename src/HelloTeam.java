59cdcd7f2ce214abed04cf56d3df1b7aa1dec15a
twzlr <twzlr@192.168.0.39>
2017-09-17 19:46:26 -0600

    All files PA1

---
+++ src/HelloTeam.java
@@ -0,0 +1,7 @@
+
+public class HelloTeam {
+	public static void main(String[] args){
+		System.out.println("Hello Team!!!");
+		System.out.println("This code is here to help with the first commit/push...");
+	}
+}
